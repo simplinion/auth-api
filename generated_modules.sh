1875ca210a0dea2bae54faaaa0d1c9dc6f5098f3
Patryk Kubiak <patryk.kubiak@choco-technologies.com>
2021-03-18 18:05:39 +0100

    #38 - Add support of qt5cpp generator
    reopen #38
    
    Changes:
    
        - Added qt5cpp generator
    
    Signed-by:
        Patryk Kubiak <patryk.kubiak90@gmail.com>

--- generated_modules.sh
+++ generated_modules.sh
@@ -2,8 +2,9 @@
 
 GENERATED_BASE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-declare -a GENERATED_MODULES_NAMES=(php-symfony php)
+declare -a GENERATED_MODULES_NAMES=(php-symfony php qt5cpp)
 declare -A GENERATED_MODULES_DIRECTORIES=(
     [php-symfony]=SymfonyBundle-php
     [php]=SwaggerClient-php
+    [qt5cpp]=qt5cpp
 )
